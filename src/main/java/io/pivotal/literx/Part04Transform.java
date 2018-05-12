b7ce3c9f69ba9275a63b3937d6036b13d2972308
aquaraga <aquaraga@gmail.com>
2018-05-12 15:19:19 +0530

    Transformation

--- src/main/java/io/pivotal/literx/Part04Transform.java
+++ src/main/java/io/pivotal/literx/Part04Transform.java
@@ -15,21 +15,24 @@ public class Part04Transform {
 
 	// TODO Capitalize the user username, firstname and lastname
 	Mono<User> capitalizeOne(Mono<User> mono) {
-		return null;
+		return mono.map(x -> new User(x.getUsername().toUpperCase(),
+				x.getFirstname().toUpperCase(), x.getLastname().toUpperCase()));
 	}
 
 //========================================================================================
 
 	// TODO Capitalize the users username, firstName and lastName
 	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.map(x -> new User(x.getUsername().toUpperCase(),
+				x.getFirstname().toUpperCase(), x.getLastname().toUpperCase()));
+
 	}
 
 //========================================================================================
 
 	// TODO Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
 	Flux<User> asyncCapitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.flatMap(this::asyncCapitalizeUser);
 	}
 
 	Mono<User> asyncCapitalizeUser(User u) {
